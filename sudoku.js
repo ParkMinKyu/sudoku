a8881535e352dee4d5630013719f3e79cdd6a955
ParkMinkyu <niee@naver.com>
2017-11-16 19:23:13 +0900

    Add files via upload

--- sudoku.js
+++ sudoku.js
@@ -16,14 +16,14 @@ var map = [[0,0,0,0,0,0,0,0,0],
 			for(let x = 0 ; x < 9 ; x ++){
 				let number = createNumber();
 				for(let y = 0 ; y < 9 ;  y ++){
-					if(checkNum(x, y, number)){
+					if(checkNumber(x, y, number)){
 						map[x][y]=number;
 					}
 				}
 			}
 			count++
 			isCreate = isComplete();
-			if(count > 10000){
+			if(count > 5000){
 				console.log("create map fail retry......")
 				count = 0 ;
 				resetMap();
@@ -32,18 +32,14 @@ var map = [[0,0,0,0,0,0,0,0,0],
 		console.log(map)
 	}
 	function resetMap(){
-		for(let x = 0 ; x < 9 ; x ++){
-			for(let y = 0 ; y < 9 ;  y ++){
-				map[x][y] = 0;
-			}
-		}
+		map.forEach(function(obj){
+			obj.fill(0)
+		});
 	}
 	function isComplete(){
 		for(let x = 0 ; x < 9 ; x ++){
-			for(let y = 0 ; y < 9 ;  y ++){
-				if(map[x][y] == 0){
-					return false;
-				}
+			if(map[x].indexOf(0) != -1){
+				return false;
 			}
 		}
 		return true;
@@ -51,13 +47,9 @@ var map = [[0,0,0,0,0,0,0,0,0],
 	function createNumber(){
 		return parseInt(Math.random()*9)+1;
 	}
-	function checkNum(x, y, number){		
+	function checkNumber(x, y, number){		
 		for(let i = 0 ; i < 9 ; i ++){
-			if(map[x][i] == number)return false;
-		}
-		
-		for(let i = 0 ; i < 9 ; i ++){
-			if(map[i][y] == number)return false;
+			if(map[x][i] == number || map[i][y] == number )return false;
 		}
 		
 		let ckxst = parseInt(x/3)*3;
@@ -87,11 +79,13 @@ var map = [[0,0,0,0,0,0,0,0,0],
 	function shuffleMap(count){
 		for(let cnt = 0 ; cnt < count ; cnt ++){
 			let poy1 = parseInt(Math.random()*9);
-      let poy2 = 0;
-      if(poy1 % 3 == 0) poy2 = (poy1 + (parseInt(Math.random()*2)+1));
-      else if(poy1 % 3 == 1)poy2 = (parseInt(Math.random()*2) == 0 ? (poy1 - 1) : (poy1 + 1));
-      else if(poy1 % 3 == 2)poy2 = (poy1 - (parseInt(Math.random()*2)+1));
-      else console.log('shuffleMap error');
+			let poy2 = 0;
+			
+			if(poy1 % 3 == 0) poy2 = (poy1 + (parseInt(Math.random()*2)+1));
+			else if(poy1 % 3 == 1)poy2 = (parseInt(Math.random()*2) == 0 ? (poy1 - 1) : (poy1 + 1));
+			else if(poy1 % 3 == 2)poy2 = (poy1 - (parseInt(Math.random()*2)+1));
+			else console.log('shuffleMap error');
+			
 			for(let x = 0 ; x < 9 ; x ++){
 				let temp = map[x][poy1];
 				map[x][poy1] = map[x][poy2];
