41cc2d5a8918115de19a6b092fa31f3b553509f7
ParkMinkyu <niee@naver.com>
2019-04-30 10:51:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,10 @@ $(function(){
 		$('#hardBtn').css('background-color','#ddd');
 	});
 	$('#undoBtn').click(function(){
-		document.execCommand('undo');
+		var history = historyStack.pop();
+		setHintArea(history.x, history.y);
+		$('.point').text('');
+		checkUserInput();
 	});
 	$('#redoBtn').click(function(){
 		document.execCommand('redo');
